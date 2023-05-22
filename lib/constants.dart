54bd3e0a942697b5918b3d5e6cf14ddadac24dcf
fidelis <fidelisnwabisi@gmail.com>
2023-05-22 14:33:16 +0100

    Added constants.dart file

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryLightolor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E)],
+);
+const kSecondaryColor = Color(0xff979797);
+const kTextColor = Color(0x77757575);
+
+const kAnimationDuration = Duration(microseconds: 200);
