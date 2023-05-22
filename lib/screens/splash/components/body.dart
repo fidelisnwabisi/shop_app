c9b1c70b0ce7e8fad2294e88782fbb54c4e03d6e
fidelis <fidelisnwabisi@gmail.com>
2023-05-22 15:49:52 +0100

    added splash_screen, body  and a couple of other folders

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
