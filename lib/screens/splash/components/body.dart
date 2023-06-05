a4498453e767bec6db45ea72565dacfb59cfb4e3
fidelis <fidelisnwabisi@gmail.com>
2023-06-06 00:07:24 +0100

    running into problems with pubspec.yaml Error: unable to locate asset entry in pubspec.yaml: "/assets/fonts/muli/Muli.ttf". Target debug_android_application failed: Exception: Failed to bundle asset files.

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -10,6 +10,14 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
   }
 }
