097c8ad25b927a036d3ec3af4c59d9f47f1c92a6
fidelis <fidelisnwabisi@gmail.com>
2023-06-06 00:23:02 +0100

    commit

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -7,6 +7,8 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Body(),
+    );
   }
 }
