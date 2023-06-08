3638425656b06bca7e26c82403f551908c674b99
fidelis <fidelisnwabisi@gmail.com>
2023-06-08 05:32:30 +0100

    adding sign form

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -9,22 +10,67 @@ class Body extends StatelessWidget {
     return SafeArea(
       child: SizedBox(
         width: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              "Welcome Back",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: getProportionateScreenWidth(28),
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Sign in with your Email and Password \nor continue with Social Media",
+                textAlign: TextAlign.center,
+              ),
+              SignForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignForm extends StatefulWidget {
+  const SignForm({super.key});
+
+  @override
+  State<SignForm> createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Email",
+              hintText: "Enter your Email",
+              // If you are using latest version of flutter then label text and hint text shown like this
+              // If you are using flutter less than 1.20.* then maybe this will not work properly
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+                borderSide: BorderSide(color: kTextColor),
+                gapPadding: 10,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+                borderSide: BorderSide(color: kTextColor),
+                gapPadding: 10,
               ),
             ),
-            Text(
-              "Sign in with your Email and Password \nor continue with Social Media",
-              textAlign: TextAlign.center,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
