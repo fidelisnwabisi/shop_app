f047d8c76e528074e85140f36d2d82e593447846
fidelis <fidelisnwabisi@gmail.com>
2023-06-10 01:03:10 +0100

    commit

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -5,6 +5,6 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
--- lib/screens/forgot_password/forgot_password_screen.dart
+++ lib/screens/forgot_password/forgot_password_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class ForgotPasswordScreen extends StatelessWidget {
   const ForgotPasswordScreen({super.key});
+
   static String routeName = "/forgot_password";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forgot Password"),
+      ),
+      body: Body(),
+    );
   }
 }
