538246976076eac1df0372629028adc522c73cb6
fidelis <fidelisnwabisi@gmail.com>
2023-06-13 01:50:24 +0100

    added Complete Profile form

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -65,38 +65,115 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
             EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
         child: Column(
           children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (newValue) => firstName = newValue!,
-              onChanged: (value) {
-                if (value.isNotEmpty) {
-                  removeError(error: kEmailNullError);
-                } else if (emailValidatorRegExp.hasMatch(value)) {
-                  removeError(error: kInvalidEmailError);
-                }
-                return null;
-              },
-              validator: (value) {
-                if (value!.isEmpty) {
-                  addError(error: kEmailNullError);
-                  return "";
-                } else if (!emailValidatorRegExp.hasMatch(value)) {
-                  addError(error: kInvalidEmailError);
-                  return "";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                labelText: "First Name",
-                hintText: "Enter your First Name",
-                // If  you are using latest version of flutter then lable text and hint text shown like this
-                // if you r using flutter less then 1.20.* then maybe this is not working properly
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg"),
-              ),
-            )
+            buildFirstNameFormField(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            buildLastNameFormField(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            buildPhoneNumberFormField(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            buildAddressFormField(),
           ],
         ),
+      ),
+    );
+  }
+
+  TextFormField buildAddressFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) => address = newValue!,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kAddressNullError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kAddressNullError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Address",
+        hintText: "Enter your Address",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon:
+            CustomSuffixIcon(svgIcon: "assets/icons/Location point.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildPhoneNumberFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      onSaved: (newValue) => phoneNumber = newValue!,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPhoneNumberNullError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kPhoneNumberNullError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Phone Number",
+        hintText: "Enter your Phone Number",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/Phone.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildLastNameFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) => lastName = newValue!,
+      decoration: InputDecoration(
+        labelText: "Last Name",
+        hintText: "Enter your Last Name",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildFirstNameFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) => firstName = newValue!,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kNameNullError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kNameNullError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "First Name",
+        hintText: "Enter your First Name",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg"),
       ),
     );
   }
