0d9e2aae374c09b4c527553f953705b40d902885
fidelis <fidelisnwabisi@gmail.com>
2023-06-13 02:55:21 +0100

    added buildTimer()

--- lib/screens/otp_screen/components/body.dart
+++ lib/screens/otp_screen/components/body.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Text(
+            "OTP Verification",
+            style: headingStyle,
+          ),
+          Text("We sent your code to +234 809 416 ****"),
+          buildTimer()
+        ],
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("This code will expire in "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 30.0, end: 0),
+          duration: Duration(seconds: 30), // Because we allow user 30seconds
+          builder: (context, value, child) => Text(
+            "${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+          onEnd: () {},
+        ),
+      ],
+    );
   }
 }
