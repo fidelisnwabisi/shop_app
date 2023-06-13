73b42f83b83943eb2352cf17e1be07cea258e1b5
fidelis <fidelisnwabisi@gmail.com>
2023-06-13 01:13:19 +0100

    added heading text

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Text(
+            "Complete Profile",
+            style: headingStyle,
+          ),
+          Text(
+            "Complete your details or continue \nwith Social Media",
+            textAlign: TextAlign.center,
+          ),
+          CompleteProfileForm()
+        ],
+      ),
+    );
+  }
+}
+
+class CompleteProfileForm extends StatefulWidget {
+  const CompleteProfileForm({super.key});
+
+  @override
+  State<CompleteProfileForm> createState() => _CompleteProfileFormState();
+}
+
+class _CompleteProfileFormState extends State<CompleteProfileForm> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
