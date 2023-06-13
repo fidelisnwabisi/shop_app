1d9ef55cc62b0cd0723d411ac2203c0d94e678ea
fidelis <fidelisnwabisi@gmail.com>
2023-06-13 02:09:39 +0100

    added terms and conditions text with height spacing around

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
 
 import 'complete_profile_form.dart';
 
@@ -10,18 +11,27 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            "Complete Profile",
-            style: headingStyle,
-          ),
-          Text(
-            "Complete your details or continue \nwith Social Media",
-            textAlign: TextAlign.center,
-          ),
-          CompleteProfileForm()
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: SizeConfig.screenHeight * 0.02), // 2%
+            Text(
+              "Complete Profile",
+              style: headingStyle,
+            ),
+            Text(
+              "Complete your details or continue \nwith Social Media",
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: SizeConfig.screenHeight * 0.05), // 5%
+            CompleteProfileForm(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            Text(
+              "By Continuing you confirm that you agree \neith our Terms and Conditions",
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
       ),
     );
   }
