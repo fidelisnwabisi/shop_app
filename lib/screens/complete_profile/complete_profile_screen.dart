fa13785768c9af07ed90a43e9edee9726f96282f
fidelis <fidelisnwabisi@gmail.com>
2023-06-13 01:07:07 +0100

    added complete_peofile_screen

--- lib/screens/complete_profile/complete_profile_screen.dart
+++ lib/screens/complete_profile/complete_profile_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class CompleteProfileScreen extends StatelessWidget {
   const CompleteProfileScreen({super.key});
   static String routeName = "/complete_profile";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Complete Sign Up"),
+      ),
+      body: Body(),
+    );
   }
 }
