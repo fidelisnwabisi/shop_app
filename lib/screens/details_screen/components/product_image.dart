8e79ce9e35c5b729870121c36e3a289bc1d80ccb
fidelis <fidelisnwabisi@gmail.com>
2023-06-17 04:20:19 +0100

    added product_image.dart imported all required imports

---
+++ lib/screens/details_screen/components/product_image.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/size_config.dart';
+
+class ProductImage extends StatefulWidget {
+  const ProductImage({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  State<ProductImage> createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  int selectedImage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: getProportionateScreenWidth(238),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(widget.product.images[selectedImage]),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ...List.generate(
+              widget.product.images.length,
+              (index) => buildSmallPreview(index),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  GestureDetector buildSmallPreview(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedImage = index;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: getProportionateScreenWidth(15)),
+        padding: EdgeInsets.all(getProportionateScreenHeight(8)),
+        height: getProportionateScreenWidth(48),
+        width: getProportionateScreenWidth(48),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color:
+                  selectedImage == index ? kPrimaryColor : Colors.transparent),
+        ),
+        child: Image.asset(widget.product.images[index]),
+      ),
+    );
+  }
+}
