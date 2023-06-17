9cb95ebb42dd49a63096b87199c0cc734017f4d2
fidelis <fidelisnwabisi@gmail.com>
2023-06-17 10:14:52 +0100

    added TopRoundedContainer widget, title and favourite icon

--- lib/screens/details_screen/components/body.dart
+++ lib/screens/details_screen/components/body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/product.dart';
 import 'package:shop_app/size_config.dart';
-
 import 'product_image.dart';
 
 class Body extends StatelessWidget {
@@ -11,6 +11,78 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProductImage(product: product);
+    return Column(
+      children: [
+        ProductImage(product: product),
+        TopRoundedContainer(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Text(
+                  product.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+                  width: getProportionateScreenWidth(64),
+                  decoration: BoxDecoration(
+                    color: product.isFavourite
+                        ? Color(0xFFFFE6E6)
+                        : Color(0xFFF5F6F9),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    ),
+                  ),
+                  child: SvgPicture.asset(
+                    "assets/icons/Heart Icon_2.svg",
+                    color: product.isFavourite
+                        ? Color(0xFFFFF4848)
+                        : Color(0xFFDBDEE4),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TopRoundedContainer extends StatelessWidget {
+  const TopRoundedContainer({
+    super.key,
+    required this.color,
+    required this.child,
+  });
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: getProportionateScreenWidth(20)),
+      padding: EdgeInsets.only(top: getProportionateScreenWidth(20)),
+      width: double.infinity,
+      height: 300,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: child,
+    );
   }
 }
