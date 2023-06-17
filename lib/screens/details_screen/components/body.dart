5416056c27e308fb98f90c33d11c5b4c6b9f859d
fidelis <fidelisnwabisi@gmail.com>
2023-06-17 10:47:07 +0100

    created product_description.dart and imported ProductDescription

--- lib/screens/details_screen/components/body.dart
+++ lib/screens/details_screen/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/product.dart';
 import 'package:shop_app/size_config.dart';
+import 'product_description.dart';
 import 'product_image.dart';
 
 class Body extends StatelessWidget {
@@ -16,41 +17,9 @@ class Body extends StatelessWidget {
         ProductImage(product: product),
         TopRoundedContainer(
           color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(20)),
-                child: Text(
-                  product.title,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: EdgeInsets.all(getProportionateScreenWidth(15)),
-                  width: getProportionateScreenWidth(64),
-                  decoration: BoxDecoration(
-                    color: product.isFavourite
-                        ? Color(0xFFFFE6E6)
-                        : Color(0xFFF5F6F9),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/icons/Heart Icon_2.svg",
-                    color: product.isFavourite
-                        ? Color(0xFFFFF4848)
-                        : Color(0xFFDBDEE4),
-                  ),
-                ),
-              )
-            ],
+          child: ProductDescription(
+            product: product,
+            pressOnSeeMore: () {},
           ),
         ),
       ],
