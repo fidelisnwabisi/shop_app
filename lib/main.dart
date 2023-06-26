904d0692ba1c38d62cd11d982fb4464beb9960e0
fidelis <fidelisnwabisi@gmail.com>
2023-06-26 08:52:50 +0100

    fixing bugs

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// void main() async {
+//   await Supabase.initialize(
+//     url: 'https://ndzhuakvuqojqlthdogk.supabase.co',
+//     anonKey:
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kemh1YWt2dXFvanFsdGhkb2drIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODczMDMzMDAsImV4cCI6MjAwMjg3OTMwMH0.Kzc9p_yFkDISbwcsBC5LvalGNRW93QHMZyaMtcNocug',
+//     authFlowType: AuthFlowType.pkce,
+//   );
+//   runApp(const MyApp());
+// }
+
+// // final supabase = Supabase.instance.client;
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: theme(),
+//       // home: SplashScreen(),
+//       // We use routName so we don't need to remember the name
+//       initialRoute: SplashScreen.routeName,
+//       routes: routes,
+//     );
+//   }
+// }
