80cab4a08091bf429461e0b6979fffd533837962
fidelis <fidelisnwabisi@gmail.com>
2023-06-27 08:45:14 +0100

    extracted CheckOutCard widget

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -12,20 +12,7 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: Body(),
-      bottomNavigationBar: Container(
-        height: 174,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, -15),
-                blurRadius: 20,
-                color: Color(0xFFDADADA)),
-          ],
-        ),
-      ),
+      bottomNavigationBar: CheckOutCard(),
     );
   }
 
@@ -43,6 +30,31 @@ class CartScreen extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class CheckOutCard extends StatelessWidget {
+  const CheckOutCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 174,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+        boxShadow: [
+          BoxShadow(
+              offset: Offset(0, -15),
+              blurRadius: 20,
+              color: Color(0xFFDADADA).withOpacity(0.15)),
+        ],
+      ),
+      child: Column(children: []),
     );
   }
 }
