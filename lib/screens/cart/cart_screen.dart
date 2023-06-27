d27dcdf38b080758d8b817281d4403a2cd236ef0
fidelis <fidelisnwabisi@gmail.com>
2023-06-27 09:15:49 +0100

    added checkout_card

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shop_app/components/default_button.dart';
-import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/cart.dart';
-import 'package:shop_app/size_config.dart';
+import 'package:shop_app/screens/cart/components/checkout_card.dart';
 import 'components/body.dart';
 
 class CartScreen extends StatelessWidget {
@@ -33,84 +30,6 @@ class CartScreen extends StatelessWidget {
             style: Theme.of(context).textTheme.bodySmall,
           )
         ],
-      ),
-    );
-  }
-}
-
-class CheckOutCard extends StatelessWidget {
-  const CheckOutCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          vertical: getProportionateScreenWidth(15),
-          horizontal: getProportionateScreenWidth(30)),
-      // We use height only for demo
-      // height: 174,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-        boxShadow: [
-          BoxShadow(
-              offset: Offset(0, -15),
-              blurRadius: 20,
-              color: Color(0xFFDADADA).withOpacity(0.15)),
-        ],
-      ),
-      child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  height: getProportionateScreenWidth(40),
-                  width: getProportionateScreenWidth(40),
-                  decoration: BoxDecoration(
-                      color: Color(0xFFF5F6F9),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: SvgPicture.asset("assets/icons/receipt.svg"),
-                ),
-                Spacer(),
-                Text("Add voucher code"),
-                // Here width is fixed that's why we use const
-                const SizedBox(width: 10),
-                Icon(
-                  Icons.arrow_forward_ios,
-                  size: 12,
-                  color: kTextColor,
-                )
-              ],
-            ),
-            SizedBox(height: getProportionateScreenWidth(20)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text.rich(
-                  TextSpan(
-                    text: "Total:\n",
-                    children: [
-                      TextSpan(
-                        text: "\$337.15",
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: getProportionateScreenWidth(190),
-                  child: DefaultButton(text: "Check Out", press: () {}),
-                ),
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
