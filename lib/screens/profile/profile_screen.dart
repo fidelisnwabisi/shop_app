6655d019f3bd96daa6eec420f6f550104df4bbef
fidelis <fidelisnwabisi@gmail.com>
2023-06-30 18:25:31 +0100

    added bottomNavigationBar

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/body.dart';
 
@@ -12,6 +13,45 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Profile")),
       body: Body(),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(vertical: 14),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, -15),
+                blurRadius: 20,
+                color: Color(0xFFDADADA).withOpacity(0.15)),
+          ],
+        ),
+        child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Shop Icon.svg"),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Heart Icon.svg"),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Chat bubble Icon.svg"),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/User Icon.svg"),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
